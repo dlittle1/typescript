0a2e894fda2418ffe04e18aa2f24b2f8d90e0646
Dylan Little <dylan.c.little@gmail.com>
2022-03-27 15:58:30 -0600

    add maps to app

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -2,8 +2,10 @@
 import { Company } from './Company';
 import { User } from './User';
 
-const user = new User();
-console.log(user);
-
-const company = new Company();
-console.log(company);
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
